eca6ebaf91498160e137a72ad20856a639a40cd5
Lycend <142054445+Lycend@users.noreply.github.com>
2025-03-09 13:20:50 +0300

    edsa

---
+++ MyCoursesR/src/Data.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import html from "./images/html.png"
+import css from "./images/css.jpg"
+import js from "./images/js.png"
+import react from "./images/react.png"
+
+export const courses = [
+  {
+    id: 147,
+    title: "Html Dili Eğitimi",
+    descripiton: "lorem input add new train system lorem " +
+      "input add new train system lorem input add new train system",
+    price: 5487,
+    link: "mycourses.com/courses/html"
+  , image: html
+  },
+  {
+    id: 754,
+    title: "Css Dili Eğitimi",
+    descripiton: "lorem input add new train system lorem " +
+      "input add new train system lorem input add new train system",
+    price: 7520,
+    link: "mycourses.com/courses/css"
+    , image: css
+  },
+  {
+    id: 854,
+    title: "Js Dili Eğitimi",
+    descripiton: "lorem input add new train system lorem " +
+      "input add new train system lorem input add new train system",
+    price: 60845,
+    link: "mycourses.com/courses/js"
+    , image: js
+  },
+  {
+    id: 452,
+    title: "React Dili Eğitimi",
+    descripiton: "lorem input add new train system lorem " +
+      "input add new train system lorem input add new train system",
+    price: 90852,
+    link: "mycourses.com/courses/react"
+    , image: react
+  }
+]
